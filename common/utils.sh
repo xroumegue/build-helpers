ad5b97e91d84ca7a9770b228568b8ddadd497e9d
Xavier Roumegue <xroumegue@gmail.com>
2022-08-15 19:51:49 +0200

    common: Add utils.sh
    Signed-off-by: Xavier Roumegue <xroumegue@gmail.com>

---
+++ common/utils.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+
+function log {
+    local msg
+    msg="$1"
+
+    if [ -n "${verbose}" ] && $verbose;
+    then
+        echo "$msg"
+    fi
+}
+
+function fatal {
+    local msg
+    msg="$1"
+
+    echo "$msg"
+    exit 1
+}
+
+function get_default {
+    if [ -z ${buildenv+x} ];
+    then
+        buildenv=default
+    fi
+
+    # shellcheck disable=SC2154
+    envfile="${rootdir}"/etc/env-"${buildenv}".sh
+
+    if [ ! -e "${envfile}" ];
+    then
+        fatal "Environment ${envfile} file not found"
+    fi
+
+    # shellcheck disable=SC1090
+    . "${envfile}"
+}
+
+get_default
